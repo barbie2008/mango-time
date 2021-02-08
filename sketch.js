5b25616ce66188cdbb9a1d73e49aa11c83f9260f
barbie2008 <77101761+barbie2008@users.noreply.github.com>
2021-02-08 20:47:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1 , mango2,  mango3     ,  mango4;     
+var world,boy;
+
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+	mango2=new mango(1100,100,33);
+	mango3=new mango(1100,100,36);
+	mango4=new mango(1100,100,39);
+
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+
+  treeObj.display();
+
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+
+  groundObject.display();
+}
